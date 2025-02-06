82666fd8201655ce97463da8615e08c85e2c5e5b
YukaNakamura10 <yuflower26@icloud.com>
2025-02-06 17:41:58 -0500

    initial spring boot set up

---
+++ backend/resHub/src/main/java/com/_7/reshub/reshub/ReshubApplication.java
@@ -0,0 +1,22 @@
+package com._7.reshub.reshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class ReshubApplication {
+
+	@GetMapping("/hello")
+    public String check() {
+        return "Hello World";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReshubApplication.class, args);
+	}
+
+}
---
+++ backend/resHub/src/test/java/com/_7/reshub/reshub/ReshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com._7.reshub.reshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

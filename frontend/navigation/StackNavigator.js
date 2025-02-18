b3ed5092ec22a9718114a69567ce17f90453cad1
Anushka Nilangekar <anushkasn17@gmail.com>
2025-02-17 23:45:40 -0500

    changed screens and color

--- frontend/navigation/StackNavigator.js
+++ frontend/navigation/StackNavigator.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import DetailsScreen from "../screens/DetailsScreen";
+import SwipeScreen from "../screens/SwipeScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ const StackNavigator = () => {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Details" component={DetailsScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="SwipeScreen" component={SwipeScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 };
